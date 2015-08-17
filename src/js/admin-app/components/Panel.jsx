59e90f6455bd61ce6a00af4fec43432effaf5c59
bmagnantb <bmagnantb@gmail.com>
2015-08-17 15:36:23 -0500

    Panel styles

--- src/js/admin-app/components/Panel.jsx
+++ src/js/admin-app/components/Panel.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router'
 
+require('./Panel.scss')
+
 export default class Panel {
 	render() {
 		return (
-			<div>
+			<div className="panel">
 				<Link to="/admin/new-post/vlog">New Video</Link>
 				<Link to="/admin/new-post/blog">New Article</Link>
 			</div>
