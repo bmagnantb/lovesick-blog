52c68ab16b6c7c6cd7af76233d112b3ae443d6ff
bmagnantb <bmagnantb@gmail.com>
2015-08-16 22:53:30 -0500

    webpack hot load react & scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,47 @@
+var config = require('config')
+var webpack = require('webpack')
 var autoprefixer = require('autoprefixer-core')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 var isDev = process.env.NODE_ENV === 'development' ? true : false
 
+var devServer = {
+	client: 'webpack-dev-server/client?' + config.get('staticFileUrl'),
+	server: 'webpack/hot/only-dev-server'
+}
+
 module.exports = {
+	devServer: {
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		contentBase: './build',
+		hot: true
+	},
 	entry: {
-		"./": isDev ? ['webpack/hot/dev-server', './src/js/client/app.js'] : './src/js/client/app.js',
-		"admin/": isDev ? ['webpack/hot/dev-server', './src/js/client/admin.js'] : './src/js/client/admin.js',
-		"admin/login/": isDev ? ['webpack/hot/dev-server', './src/js/client/login.js'] : './src/js/client/login.js'
+		"./": isDev
+			? [devServer.client, devServer.server, './src/js/client/app.js']
+			: './src/js/client/app.js',
+		"admin/": isDev
+			? [devServer.client, devServer.server, './src/js/client/admin.js']
+			: './src/js/client/admin.js',
+		"admin/login/": isDev
+			? [devServer.client, devServer.server, './src/js/client/login.js']
+			: './src/js/client/login.js'
 	},
 	output: {
 		path: __dirname + '/build',
-		filename: '[name]app.js'
+		filename: '[name]app.js',
+		publicPath: config.get('staticFileUrl') + '/'
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.jsx?$/,
 				exclude: /(node_modules)/,
-				loader: 'babel'
+				loaders: ['react-hot', 'babel']
 			},
 			{
 				test: /\.scss$/,
-				loader: process.env.NODE_ENV === 'development'
+				loader: isDev
 					? 'style!css!postcss!sass'
 					: ExtractTextPlugin.extract('style', 'css!postcss!sass')
 			}
@@ -32,6 +51,8 @@ module.exports = {
 		}
 	},
 	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
 		new ExtractTextPlugin('[name]style.css')
 	],
 	resolve: {
