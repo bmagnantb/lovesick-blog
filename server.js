a8556bf9cb6d9c3b6b2908d94bc265d3e294b1aa
bmagnantb <bmagnantb@gmail.com>
2015-08-16 23:27:56 -0500

    rewrite server entry in es5 for babel require hook on deploy instead of babel-node

--- server.js
+++ server.js
@@ -1,45 +1,52 @@
-import piping from 'piping'
-import express from 'express'
-import passport from 'passport'
-import session from 'express-session'
-
-import addRenderEngine from './src/js/server/addRenderEngine'
-import { initPassport, authMiddleware } from './src/js/server/utils'
+var piping = require('piping')
 
 if (process.env.NODE_ENV === 'production' || piping({hook: true})) {
 
+	require('babel/register')
+	var config = require('config')
+	var express = require('express')
+	var passport = require('passport')
+	var session = require('express-session')
+
+	var addRenderEngine = require('./src/js/server/addRenderEngine')
+	var utils = require('./src/js/server/utils')
+	var initPassport = utils.initPassport
+	var authMiddleware = utils.authMiddleware
+
 	// prevent scss requires, must import routes after
-	require.extensions['.scss'] = () => {}
-	var { admin, adminLogin, main } = require('./src/js/server/routes')
+	require.extensions['.scss'] = function() {}
+	var routes = require('./src/js/server/routes')
 
 	var app = express()
 
 	addRenderEngine(app)
 	initPassport(passport)
 
-	app.use('*/favicon.ico', (req, res, next) => {
+	app.use('*/favicon.ico', function(req, res, next) {
 		res.sendStatus(404)
 	})
 
 	app.use('/admin/login',
 		express.static(__dirname + '/build/admin/login'),
-		adminLogin
+		routes.adminLogin
 	)
 
 	app.use('/admin(?!/login)',
 		authMiddleware.session,
 		authMiddleware.pInit,
 		authMiddleware.pSession,
-		(req, res, next) => req.isAuthenticated() ? next() : res.redirect('/admin/login'),
+		function(req, res, next) {
+			req.isAuthenticated() ? next() : res.redirect('/admin/login')
+		},
 		express.static(__dirname + '/build/admin'),
-		admin
+		routes.admin
 	)
 
 	app.use(express.static(__dirname + '/build'))
 
-	app.use('/', main)
+	app.use('/', routes.main)
 
-	app.listen(process.env.PORT || 3000, () => {
-		console.log('apping')
+	app.listen(process.env.PORT || config.get('port'), function() {
+		console.log('servered')
 	})
 }
