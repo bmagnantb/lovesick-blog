becd69753d4e2406e8dbc8cc6c6f5f136787a32f
bmagnantb <bmagnantb@gmail.com>
2015-08-31 11:15:47 -0500

    fixed newPost route to include req.query and lowercase route

--- src/js/server/routes/admin/newPost.js
+++ src/js/server/routes/admin/newPost.js
@@ -1,14 +1,14 @@
 export default function newPost(req, res) {
 
 	var now = new Date()
-	var newPost = Object.assign({}, {
+	var newPost = Object.assign({}, req.query, {
 		timestamp: Date.parse(now),
 		date: {
 			year: now.getFullYear(),
 			month: now.getMonth(),
 			day: now.getDate()
 		},
-		route: req.query.title.replace(/ /g, '-')
+		route: req.query.title.toLowerCase().replace(/ /g, '-')
 	})
 
 	req.mongoDb.collection('posts').insertOne(newPost, (err, resp) => {
